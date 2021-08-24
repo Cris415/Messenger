bb0b5d41f00477e793bfafa9349fb2775c4bd1df
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-08-23 21:12:46 -0700

    Remove reduce function

--- client/src/components/Sidebar/UnreadBadge.js
+++ client/src/components/Sidebar/UnreadBadge.js
@@ -6,32 +6,14 @@ const useStyles = makeStyles((theme) => ({
   Badge: { fontWeight: "bolder" },
 }));
 
-// count unread messages based on the time the conversation was last read
-// function accepts conversation object and returns a count (number)
-export const countUnread = (conversation) => {
-  const { otherUser, messages, lastRead } = conversation;
-
-  return messages.reduce((acc, curr) => {
-    const lastReadDate = new Date(lastRead).getTime();
-    const currConvoDate = new Date(curr.createdAt).getTime();
-
-    // Count messages that belong to other user and sent after last read conversation
-    if (otherUser.id === curr.senderId && lastReadDate < currConvoDate) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-};
-
-const UnreadChat = (props) => {
+const UnreadBadge = (props) => {
   const classes = useStyles();
   const { conversation } = props;
-  const count = countUnread(conversation);
   return (
     <div className={classes.root}>
       <Badge
         color="primary"
-        badgeContent={count}
+        badgeContent={conversation.unreadMessageCount}
         max={999}
         classes={{ badge: classes.Badge }}
       />
@@ -39,4 +21,4 @@ const UnreadChat = (props) => {
   );
 };
 
-export default UnreadChat;
+export default UnreadBadge;
