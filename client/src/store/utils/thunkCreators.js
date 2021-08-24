41ee44fc7787cf626d9ddb908d121e15eb69618b
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-08-23 21:36:22 -0700

    Update names

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -5,7 +5,7 @@ import {
   addConversation,
   setNewMessage,
   setSearchedUsers,
-  updatedLastRead
+  addLastReadDateToStore,
 } from "../conversations";
 import { gotUser, setFetchingStatus } from "../user";
 
@@ -82,11 +82,11 @@ export const fetchConversations = () => async (dispatch) => {
 export const updateLastRead = (id) => async (dispatch) => {
   try {
     const { data } = await axios.patch(`/api/conversations/${id}`);
-    dispatch(updatedLastRead(data))
+    dispatch(addLastReadDateToStore(data));
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 const saveMessage = async (body) => {
   const { data } = await axios.post("/api/messages", body);
