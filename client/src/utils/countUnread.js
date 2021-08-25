54979a7b30fcda0cbf4e9f651bcec13d66067e80
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-08-24 17:25:52 -0700

    Update countUnread to work with lastread object

--- client/src/utils/countUnread.js
+++ client/src/utils/countUnread.js
@@ -1,17 +1,17 @@
-const countUnread = (conversation, userId, date) => {
-  const { messages } = conversation;
-
+const countUnread = (conversation) => {
+  const { messages, lastreads } = conversation;
   // iterate over messages and count the ones that have been sent after last read
+  if (!lastreads) return 0;
   return messages.reduce((acc, curr) => {
-    const lastReadDate = new Date(date).getTime();
+    const lastReadDate = new Date(lastreads.date).getTime();
     const currMessageDate = new Date(curr.createdAt).getTime();
 
     // Count messages that belong to other user and sent after last read conversation
-    if (userId !== curr.senderId && lastReadDate < currMessageDate) {
+    if (lastreads.userId !== curr.senderId && currMessageDate > lastReadDate) {
       return acc + 1;
     }
     return acc;
   }, 0);
 };
 
-module.exports = countUnread;
+export default countUnread;
