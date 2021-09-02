cb4a79f465f2040fcca638900232d8a2050c0086
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-09-01 18:17:50 -0700

    Move date outside iterator to calculate once

--- client/src/utils/countUnread.js
+++ client/src/utils/countUnread.js
@@ -2,10 +2,10 @@ const countUnread = (conversation) => {
   const { messages, lastreads } = conversation;
   // iterate over messages and count the ones that have been sent after last read
   if (!lastreads) return 0;
+  
+  const lastReadDate = new Date(lastreads.date).getTime();
   return messages.reduce((acc, curr) => {
-    const lastReadDate = new Date(lastreads.date).getTime();
     const currMessageDate = new Date(curr.createdAt).getTime();
-
     // Count messages that belong to other user and sent after last read conversation
     if (lastreads.userId !== curr.senderId && currMessageDate > lastReadDate) {
       return acc + 1;
