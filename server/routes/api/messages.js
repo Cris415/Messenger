f4465358241f60f9a3adeddd6bda43bd37468753
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-09-02 16:04:10 -0700

    Get lastread info and send it with msg

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Conversation, Message } = require("../../db/models");
+const { Conversation, Message, LastRead } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
@@ -13,8 +13,24 @@ router.post("/", async (req, res, next) => {
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
+      let lastreads = await LastRead.findAll({
+        where: { conversationId },
+      });
+      if (lastreads.length < 2) {
+        const lastreadUser1 = await LastRead.create({
+          userId: senderId,
+          conversationId,
+        });
+        const lastreadUser2 = await LastRead.create({
+          userId: recipientId,
+          conversationId,
+        });
+
+        lastreads = [lastreadUser1, lastreadUser2];
+      }
+
       const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
+      return res.json({ message, sender, lastreads });
     }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
@@ -32,12 +48,29 @@ router.post("/", async (req, res, next) => {
         sender.online = true;
       }
     }
+    let lastreads = await LastRead.findAll({
+      where: { conversationId: conversation.id },
+    });
+
+    if (lastreads.length < 2) {
+      const lastreadUser1 = await LastRead.create({
+        userId: senderId,
+        conversationId: conversation.id,
+      });
+      const lastreadUser2 = await LastRead.create({
+        userId: recipientId,
+        conversationId: conversation.id,
+      });
+
+      lastreads = [lastreadUser1, lastreadUser2];
+    }
+
     const message = await Message.create({
       senderId,
       text,
       conversationId: conversation.id,
     });
-    res.json({ message, sender });
+    res.json({ lastreads, message, sender });
   } catch (error) {
     next(error);
   }
